879f7034f2f3c8d0d48946cd2f2905c4e76e59b1
Joshua Sigston <joshua.sigston@outlook.com>
2024-05-08 19:49:55 -0700

    authentication for admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { promise } from "zod";
+import { isValidPassword } from "./lib/isValidPassword";
 
 export async function middleware (req: NextRequest) {
   if ((await isAuthenticated(req) === false)) {
@@ -11,7 +12,15 @@ export async function middleware (req: NextRequest) {
 }
 
 async function isAuthenticated (req: NextRequest) {
-  return Promise.resolve(false)
+  const authHeader = req.headers.get("authorization" || req.headers.get("Authorization"))
+
+  if (authHeader == null) return false
+
+  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+  .toString()
+  .split(":")
+  
+  return username === process.env.ADMIN_USERNAME && (await isValidPassword(password, process.env.HASHED_ADMIN_PASSWORD as string))
 }
 
 export const config = {
